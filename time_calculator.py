ed5c6b2636d6845a042ea4f88740ea848cce03d1
Bilal K. Hamada <bilalkamal2009@gmail.com>
2021-06-28 18:47:35 +0200

    Done!

--- time_calculator.py
+++ time_calculator.py
@@ -39,13 +39,4 @@ def add_time(start, duration, day = None):
     
     
 
-    # return new_time
-print(add_time("3:30 PM", "2:12", "Monday"))
-# print(add_time("11:30 AM", "2:32", "Monday"))
-# print(add_time("11:43 AM", "00:20"))
-# print(add_time("10:10 PM", "3:30"))
-# print(add_time("11:43 PM", "24:20", "tueSday"))
-# print(add_time("6:30 PM", "205:12"))
-
-
 
