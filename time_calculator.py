f52dd528bb2d8c18a4e52557b62114d417c83bfd
Bilal K. Hamada <bilalkamal2009@gmail.com>
2021-06-28 15:31:48 +0200

    done! now testing time :)

--- time_calculator.py
+++ time_calculator.py
@@ -17,6 +17,12 @@ def convert_hrs_to24(hours):
     else: return 12 + hours 
 
 
+def convert_to_12(hours):
+    if hours == 24:
+        return 0
+    else: return 12 - hours
+
+
 def add_time(start, duration, day = None):
     minutes_counter,new_hours,hours_counter = 0,0,0
     start_hours ,start_minutes, start_am_pm = get_hours_min(start)
@@ -25,13 +31,36 @@ def add_time(start, duration, day = None):
     if minutes_counter >=60: new_hours, minutes_counter = convert_min_to_hrs(minutes_counter)
     if start_am_pm == "PM": hours_counter , start_hours = convert_hrs_to24(start_hours), 0 
     hours_counter += new_hours + duration_hours + start_hours
-    
-    print(hours_counter)
-    print(minutes_counter)
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday']
     if hours_counter <11 and day is None: return f'{hours_counter%12}:{minutes_counter} AM'
     if hours_counter <24 and day is None and hours_counter >11: return f'{hours_counter%12}:{minutes_counter} PM'
     if hours_counter <11 and day is not None: return f'{hours_counter%12}:{minutes_counter} AM, {day.title()}'
     if hours_counter <24 and day is not None and hours_counter >11: return f'{hours_counter%12}:{minutes_counter} PM, {day.title()}'
-    
+    if hours_counter >= 24 and day is None:
+        if hours_counter // 24 == 1:
+            am_time = True
+            if hours_counter // 24 > 11: hours, am_time = convert_to_12(hours_counter//24), False
+            if am_time:return f'{hours_counter%24}:{minutes_counter} AM (next day)'
+            if not am_time: return f'{hours}:{minutes_counter} PM (next day)'
+        else:
+            am_time = True
+            days_past = hours_counter // 24
+            if hours_counter // 24 > 11: hours, am_time = convert_to_12(hours_counter//24), False
+            if am_time:return f'{hours_counter%24}:{minutes_counter} AM ({days_past} days later)'
+            if not am_time: return f'{hours}:{minutes_counter} PM ({days_past} days later)'
+    if hours_counter >= 24 and day is not None:
+        if hours_counter // 24 == 1:
+            am_time = True
+            if hours_counter // 24 > 11: hours, am_time = convert_to_12(hours_counter//24), False
+            if am_time:return f'{hours_counter%24}:{minutes_counter} AM (next day)'
+            if not am_time: return f'{hours}:{minutes_counter} PM (next day)'
+        else:
+            days_past = hours_counter // 24
+            am_time = True
+            if hours_counter // 24 > 11: hours, am_time = convert_to_12(hours_counter//24), False
+            if am_time:return f'{hours_counter%24}:{minutes_counter} AM, {days[days.index(day) + days_past%7]} ({days_past} days later)'
+            
+            if not am_time: return f'{hours}:{minutes_counter} PM, {days[days.index(day) + days_past%7]} ({hours_counter // 24} day)'
+
     # return new_time
-print(add_time("0:30 AM", "200:12","Monday"))
+print(add_time("3:00 PM", "3:10"))
